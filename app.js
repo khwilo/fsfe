c9f2eb6ffdfd0336de891887f3e4d31dfcf4ddf2
Khwilo Kabaka <khwilowatai@gmail.com>
2021-03-30 08:18:22 +0300

    Set a custom header

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 
 const app = express();
-const port= 3000;
+const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello to  my fellow fullstack engineer!");
+app.get('/', (req, res) => {
+  res.send('Hello to  my fellow fullstack engineer!');
+});
+
+app.get('/custom', (req, res) => {
+  res.set('X-fsfe', 'FSFE custom header');
+  res.status(418);
+  res.send('Setting a custom header works!');
 });
 
 app.listen(port, () => console.log(`App started. Listening on port: ${port}`));
-
